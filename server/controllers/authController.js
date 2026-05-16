f3da05423e433b0421a7a97faf4fbd5cd724ed2a
Anuxragg <palanurag208@gmail.com>
2026-05-16 23:43:33 +0530

    feat: implement authentication controller with registration, login, token refresh, and Google OAuth support

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,4 @@
 const User = require('../models/User');
-const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const {
   generateAccessToken,
   generateRefreshToken,
